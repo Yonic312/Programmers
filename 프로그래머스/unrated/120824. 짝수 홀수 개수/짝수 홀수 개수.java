f4319d161ae3a7528a97a1b97bbba923d9a893b9
Yonic312 <58536183+Yonic312@users.noreply.github.com>
2023-11-26 22:42:53 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                result[0] += 1;
+            } else {
+                result[1] += 1;
+            }
+        }
+        return result;
+    }
+}
