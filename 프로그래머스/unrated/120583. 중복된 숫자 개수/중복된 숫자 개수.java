3a2890d579c29fa464bbb905f01c106f7bb21119
Yonic312 <58536183+Yonic312@users.noreply.github.com>
2023-12-14 21:05:13 +0900

    [level unrated] Title: 중복된 숫자 개수, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120583. 중복된 숫자 개수/중복된 숫자 개수.java
+++ 프로그래머스/unrated/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int solution(int[] my_string, int n) { // solution이라는 함수 안에 (매개변수)
+    public int solution(int[] my_string, int n) { // solution이라는 함수 안에 (매개변수) 
         int cnt = 0;
 
         for (int num : my_string) {
