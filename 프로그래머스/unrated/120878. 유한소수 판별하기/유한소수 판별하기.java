91c0b7d67c1014c7de5dc49bffb94e4525a470ed
Yonic312 <58536183+Yonic312@users.noreply.github.com>
2023-12-23 20:59:19 +0900

    Update 유한소수 판별하기.java

--- 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,14 +1,15 @@
-public class Solution{
-public int solution(int a, int b) {
- int answer = 0;
- while(b%2==0||b%5==0){
- if(b%2==0){
- b/=2;
- }
- if(b%5==0){
- b/=5;
- }
- }
- return (a%b==0)? 1:2;
- }
+class Solution {
+    public int solution(int a, int b) {
+        // 분모 기약분수 만들기
+        while (b % 2 == 0 || b % 5 == 0) {
+            if (b % 2 == 0) {
+                b /= 2;
+            }
+            if (b % 5 == 0) {
+                b /= 5;
+            }
+        }
+        // 기약분수인 분모로 분자를 나눠봄
+        return a % b == 0 ? 1 : 2;
+    }
 }
