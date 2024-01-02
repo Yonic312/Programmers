de23752c336f16a809e6836702f71af533900d84
Yonic312 <58536183+Yonic312@users.noreply.github.com>
2024-01-02 22:00:27 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 1.18 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0, s);
+        answer += overwrite_string;
+        answer += (my_string.substring(s + overwrite_string.length(), my_string.length()));
+        
+        return answer;
+    }
+    
+}
